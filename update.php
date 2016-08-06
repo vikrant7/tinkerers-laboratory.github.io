91f4a043869f702288f797fd635543686bc89ccf
Aadhavan Nambhi M <aadhavannambhi.m@gmail.com>
2016-08-07 00:04:53 +0530

    Auto updation

--- update.php
+++ update.php
@@ -7,7 +7,7 @@ if( urldecode($_GET['auth']) !== $_SERVER['GITHUB_TINKERER_SECRET']){
   $data = json_decode($payload);
   var_dump($data->ref);
   if($data->ref === 'refs/heads/master'){
-    echo shell_exec("sudo -u root /usr/bin/webhook_update.sh https://github.com/tinkerers-laboratory/tinkerers-laboratory.github.io");
+    echo shell_exec("sudo -u root /usr/bin/webhook_update.sh https://github.com/tinkerers-laboratory/tinkerers-laboratory.github.io tinkerers-lab");
     printf('OK');
     http_response_code(200);
   } else {
